6f959347d56b8358fe17415457776cf165e5c7fb
Maiche <ammaiche@gmail.com>
2019-05-26 17:49:13 +0200

    Restructured code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {SearchComponent} from './features/search/use-case/search/search.component';
+import {SearchComponent} from './features/search/use-case/search.component';
 
 const routes: Routes = [
   {
--- src/app/features/search/search.module.ts
+++ src/app/features/search/search.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import { UserCardComponent } from './components/user-card/user-card.component';
-import { SearchComponent } from './use-case/search/search.component';
+import { SearchComponent } from './use-case/search.component';
 import { AvatarComponent } from './components/avatar/avatar.component';
 import { RouterModule } from '@angular/router';
 import { SearchFieldComponent } from './components/search-field/search-field.component';
